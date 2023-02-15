45b79d2669524436149d37aaf45798eac3d5e9f5
EnriqueSFlor <enriquesilvaflor@gmail.com>
2023-02-15 20:54:34 -0300

    ajustando java

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-const controle = document.querySelectorAll(".controle-ajuste");
+const controle = document.querySelectorAll("[data-controle]");
 
 controle.forEach((elemento) => {
   elemento.addEventListener("click", (evento) => {
-    manipulaDados(evento.target.textContent, evento.target.parentNode);
+    manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
   });
 });
 
 function manipulaDados(operacao, controle) {
-  const peca = controle.querySelector(".controle-contador");
+  const peca = controle.querySelector("[data-contador]");
 
-  if (operacao === "+") {
-    peca.value = parseInt(peca.value) + 1;
-  } else {
+  if (operacao === "-") {
     peca.value = parseInt(peca.value) - 1;
+  } else {
+    peca.value = parseInt(peca.value) + 1;
   }
 }
